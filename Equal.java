93e72859f81425e9b3c3cced9d845b8bb786c7c2
samsam059 <146161431+samsam059@users.noreply.github.com>
2024-07-13 20:32:18 +0530

    Create Equal.java

---
+++ Equal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Equal{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    String str1=sc.nextLine();
+    String str2=sc.nextLine();
+    if(str1.equals(str2))
+    {
+      System.out.print("Equal");
+    }
+    else
+    {
+      System.out.print("Not Equal");
+    }
+  }
+}
